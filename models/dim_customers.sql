20ff857de1c94e8c655bc54200221001b021ad0d
Rohit Sunku <rgs8890@gmail.com>
2025-05-28 18:58:23 +0000

    adding staging files and ref functions

--- models/dim_customers.sql
+++ models/dim_customers.sql
@@ -4,20 +4,13 @@
 
 
 with customers as (
-    select 
-        id as customer_id,
-        first_name,
-        last_name
-    from dbt_rsunku.jaffle_shop_customers
+    select *
+    from {{ ref('stg_jaffle_shop__customers') }}
 ),
 orders as
 (
-    select
-        id as order_id,
-        user_id as customer_id,
-        order_date,
-        status
-    from dbt_rsunku.jaffle_shop_orders
+    select *
+    from {{ ref('stg_jaffle_shop__orders') }}
 )
 ,
 customers_orders as
---
+++ models/stg_jaffle_shop__customers.sql
@@ -0,0 +1,5 @@
+select
+    id as customer_id,
+    first_name,
+    last_name
+from dbt_rsunku.jaffle_shop.customers
